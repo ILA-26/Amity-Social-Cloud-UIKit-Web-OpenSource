8d9ba3e646e22576db646e21a0e03ded80b18383
Abdennour Boudjema <a.boudjema@intelifox.com>
2024-04-29 21:52:21 +0400

    Removed product image from custom post display due to limitations

--- src/ila26/components/ILA26_CustomPostContent.tsx
+++ src/ila26/components/ILA26_CustomPostContent.tsx
@@ -5,37 +5,9 @@ import ILA26_MarketplaceProduct from './MarketplaceProduct';
 const ILA26_CustomPostContent = ({ metadata, dataType, data }: ILA26_CustomPostContentProps) => {
   return metadata ? (
     <a href={data.text} style={{ all: 'unset', cursor: 'pointer' }} target="_blank">
-      <div style={{ display: 'flex', flexDirection: 'column', padding: '1rem 3rem' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', padding: '0' }}>
         <div style={{ borderRadius: '8px', overflow: 'hidden', background: '#f8f8f8' }}>
-          {dataType === 'customPost.marketplaceProduct' && (
-            <div
-              style={{
-                position: 'relative',
-                width: '100%',
-                height: '100%',
-              }}
-            >
-              <img
-                src={
-                  metadata?.productPicture?.azureBlobUri ??
-                  'https://stilan26prdfront.blob.core.windows.net/b2c/logo.c116c8b3.jpg'
-                }
-                style={{ width: '100%', maxHeight: '180px', objectFit: 'cover' }}
-              />
-              <div
-                style={{
-                  position: 'absolute',
-                  background:
-                    ' linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.6629245448179272) 5%, rgba(255,255,255,0) 100%)',
-                  top: 0,
-                  left: 0,
-                  height: '98%',
-                  width: '100%',
-                }}
-              ></div>
-            </div>
-          )}
-          <div style={{ padding: '1rem 1rem' }}>
+          <div style={{ padding: '1rem 1rem 0 1rem' }}>
             {dataType === `customPost.serviceOffer` ? (
               <ILA26_ServiceOffer metadata={metadata} />
             ) : dataType === `customPost.marketplaceProduct` ? (
