7b35c5bab400e5649ab15f8e03321c0d7878083b
Abdennour Boudjema <139641657+1ntel1foxgithub@users.noreply.github.com>
2024-04-29 21:24:19 +0100

    Feature/override default fonts (#1)
    * changing global font to Avenir next (ila26 default font face)
    
    * Removed product image from custom post display due to limitations
    
    * added service link from enterprise detail edge case & address to the display

--- src/ila26/utils.ts
+++ src/ila26/utils.ts
@@ -3,23 +3,19 @@ import { ILA26_internalElementsTypes } from './types/customPosts';
 const ILA26_matchPostTextIsInternalElement = (
   textURL: string,
 ): { type: ILA26_internalElementsTypes; id: string } | false => {
-  const serviceOffersRegex = /.+store\/offers\/([^/]+)/;
-  const marketplaceProductRegex = /.+store\/catalog\/products\/([^/]+)/;
+  const serviceOffersRegex = /.+offers\/([^/]+)/;
+  const marketplaceEnterpriseServiceOfferRegex = /.+\/store\/company\/details\/.+\/offers\/([^/]+)/;
+  const marketplaceProductRegex = /.+catalog\/products\/([^/]+)/;
   const marketplaceEnterpriseProductsRegex =
     /.+\/store\/company\/details\/.+\/catalog\/products\/([^/]+)/;
 
-  console.log({
-    textURL,
-    isService: serviceOffersRegex.test(textURL),
-    id0: textURL.match(serviceOffersRegex)?.[1],
-    isMarketplace: marketplaceProductRegex.test(textURL),
-    id1: textURL.match(marketplaceProductRegex)?.[1],
-    isDetail: marketplaceEnterpriseProductsRegex.test(textURL),
-    id2: textURL.match(marketplaceEnterpriseProductsRegex)?.[1],
-  });
-
   if (serviceOffersRegex.test(textURL))
     return { type: 'serviceOffer', id: textURL.match(serviceOffersRegex)?.[1] as string };
+  if (marketplaceEnterpriseServiceOfferRegex.test(textURL))
+    return {
+      type: 'serviceOffer',
+      id: textURL.match(marketplaceEnterpriseServiceOfferRegex)?.[1] as string,
+    };
   if (marketplaceProductRegex.test(textURL))
     return {
       type: 'marketplaceProduct',
