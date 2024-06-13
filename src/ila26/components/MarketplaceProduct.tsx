fb3951820ef3abae080ba8f44d09cacb534bac2b
Yacine Bakhti <yacinebakhti1mi@gmail.com>
2024-06-14 00:48:38 +0100

    feat(ila26): display price in product post

--- src/ila26/components/MarketplaceProduct.tsx
+++ src/ila26/components/MarketplaceProduct.tsx
@@ -3,8 +3,14 @@ import React from 'react';
 import { ILA26_UiKitProviderProps } from '../types/kitProviderProps';
 import { useIntl } from 'react-intl';
 
+const formatter = new Intl.NumberFormat('fr-FR', {
+  style: 'currency',
+  currency: 'EUR',
+});
+
 const ILA26_MarketplaceProduct = ({ metadata }: ILA26_MarketplaceProductProps) => {
   const { locale } = useIntl();
+
   return (
     <>
       <span
@@ -29,6 +35,21 @@ const ILA26_MarketplaceProduct = ({ metadata }: ILA26_MarketplaceProductProps) =
         }}
       >
         {metadata?.description}
+      </p>
+
+      <p>
+        {(locale as ILA26_UiKitProviderProps['locale']) === 'en-US' ? 'Price' : 'Prix'}
+
+        <span
+          style={{
+            color: '#5C2DD3',
+            fontWeight: '700',
+            marginLeft: '5px',
+            fontSize: '17px',
+          }}
+        >
+          {formatter.format(metadata?.price || 0)}
+        </span>
       </p>
     </>
   );
