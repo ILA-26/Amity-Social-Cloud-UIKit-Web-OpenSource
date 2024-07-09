9fddf550060f9a0b0f4e438c3cc3a15a2364aac2
Abdelhak RAHMOUNI <rahmouni.abdelhak.1@gmail.com>
2024-07-09 17:21:34 +0100

    feat: make popup larger

--- src/ila26/chat/components/ChatPopup/styles.ts
+++ src/ila26/chat/components/ChatPopup/styles.ts
@@ -4,8 +4,8 @@ export const Popup = styled.div`
   position: fixed;
   bottom: 0px;
   right: 50px;
-  height: 60vh;
-  width: 25vw;
+  height: 70vh;
+  width: 30vw;
   z-index: 1;
   box-shadow: 0px 0px 5px lightgray;
   border-radius: 10px;
