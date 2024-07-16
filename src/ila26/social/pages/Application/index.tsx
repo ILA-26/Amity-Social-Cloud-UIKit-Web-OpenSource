6a2540502cafa7ef527ca672d43dbd732132430e
Abdelhak RAHMOUNI <rahmouni.abdelhak.1@gmail.com>
2024-07-16 21:29:38 +0100

    chore: port changes for AmityUiKitSocial along with dep files

---
+++ src/ila26/social/pages/Application/index.tsx
@@ -0,0 +1,96 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+
+import { PageTypes } from '~/social/constants';
+
+import MainLayout from '~/social/layouts/Main';
+
+import CommunitySideMenu from '~/social/components/CommunitySideMenu';
+
+import ExplorePage from '~/social/pages/Explore';
+import NewsFeedPage from '~/ila26/social/pages/NewsFeed';
+import CommunityFeedPage from '~/ila26/social/pages/CommunityFeed';
+import UserFeedPage from '~/ila26/social/pages/UserFeed';
+import CategoryCommunitiesPage from '~/social/pages/CategoryCommunities';
+import CommunityEditPage from '~/social/pages/CommunityEdit';
+import ProfileSettings from '~/social/components/ProfileSettings';
+import { useNavigation } from '~/social/providers/NavigationProvider';
+import useSDK from '~/core/hooks/useSDK';
+import { ILA26_internalFeedProps } from '~/ila26/types/customPosts';
+
+const ApplicationContainer = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+const StyledCommunitySideMenu = styled(CommunitySideMenu)`
+  min-height: 100%;
+`;
+
+const Community = ({
+  ILA26_getInternalData,
+  ILA26_communityManagerProps,
+}: ILA26_internalFeedProps) => {
+  const { page } = useNavigation();
+
+  const { client } = useSDK();
+  const [socialSettings, setSocialSettings] = React.useState<Amity.SocialSettings | null>(null);
+
+  useEffect(() => {
+    if (client == null) return;
+
+    async function run() {
+      const settings = await client?.getSocialSettings();
+
+      if (settings) {
+        setSocialSettings(settings);
+      }
+    }
+    run();
+  }, [client]);
+
+  return (
+    <ApplicationContainer>
+      <MainLayout aside={<StyledCommunitySideMenu activeCommunity={page.communityId} />}>
+        {page.type === PageTypes.Explore && <ExplorePage />}
+
+        {page.type === PageTypes.NewsFeed && (
+          <NewsFeedPage
+            ILA26_communityManagerProps={ILA26_communityManagerProps}
+            ILA26_getInternalData={ILA26_getInternalData}
+          />
+        )}
+
+        {page.type === PageTypes.CommunityFeed && (
+          <CommunityFeedPage
+            ILA26_communityManagerProps={ILA26_communityManagerProps}
+            ILA26_getInternalData={ILA26_getInternalData}
+            communityId={page.communityId}
+            isNewCommunity={page.isNewCommunity}
+          />
+        )}
+
+        {page.type === PageTypes.CommunityEdit && (
+          <CommunityEditPage communityId={page.communityId} tab={page.tab} />
+        )}
+
+        {page.type === PageTypes.Category && (
+          <CategoryCommunitiesPage categoryId={page.categoryId} />
+        )}
+
+        {page.type === PageTypes.UserFeed && (
+          <UserFeedPage
+            ILA26_communityManagerProps={ILA26_communityManagerProps}
+            ILA26_getInternalData={ILA26_getInternalData}
+            userId={page.userId}
+            socialSettings={socialSettings}
+          />
+        )}
+
+        {page.type === PageTypes.UserEdit && <ProfileSettings userId={page.userId} />}
+      </MainLayout>
+    </ApplicationContainer>
+  );
+};
+
+export default Community;
---
+++ src/ila26/social/pages/NewsFeed/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { PageTypes } from '~/social/constants';
+import Feed from '~/social/components/Feed';
+
+import { useNavigation } from '~/social/providers/NavigationProvider';
+
+import { Wrapper } from './styles';
+import { ILA26_internalFeedProps } from '~/ila26/types/customPosts';
+
+const NewsFeed = ({
+  ILA26_communityManagerProps,
+  ILA26_getInternalData,
+}: ILA26_internalFeedProps) => {
+  const { onChangePage } = useNavigation();
+
+  return (
+    <Wrapper data-qa-anchor="news-feed">
+      <Feed
+        targetType={'globalFeed'}
+        goToExplore={() => onChangePage(PageTypes.Explore)}
+        showPostCreator
+        ILA26_communityManagerProps={ILA26_communityManagerProps}
+        ILA26_getInternalData={ILA26_getInternalData}
+      />
+    </Wrapper>
+  );
+};
+
+export default NewsFeed;
