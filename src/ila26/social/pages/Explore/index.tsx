f20af25c551ee2c535c473c0bdc00595f72b6104
Abdelhak RAHMOUNI <rahmouni.abdelhak.1@gmail.com>
2024-11-14 12:12:57 +0100

    feat: add create new community button

--- src/ila26/social/pages/Explore/index.tsx
+++ src/ila26/social/pages/Explore/index.tsx
@@ -1,21 +1,37 @@
-import React from 'react';
-
+import React, { useCallback, useState } from 'react';
 import Recommended from '~/ila26/social/components/community/RecommendedCommunities';
 import FollowedEnterprises from '~/ila26/social/components/community/FollowedEnterprises';
-import CategoriesCard from '~/social/components/category/CategoriesCard';
-
-import { HeaderSection, PageContainer } from './styles';
 import SocialSearch from '~/ila26/social/components/SocialSearch';
+import CategoriesCard from '~/social/components/category/CategoriesCard';
+import CommunityCreationModal from '~/social/components/CommunityCreationModal';
+import { HeaderSection, PageContainer, PlusIcon } from './styles';
+import Button from '~/core/components/Button';
+import { FormattedMessage } from 'react-intl';
 
-const ExplorePage = () => (
-  <PageContainer>
-    <HeaderSection>
-      <SocialSearch sticky />
-    </HeaderSection>
-    <Recommended />
-    <CategoriesCard />
-    <FollowedEnterprises />
-  </PageContainer>
-);
+const ExplorePage = () => {
+  const [isOpen, setOpen] = useState<boolean>(false);
+
+  const handleToggleCreationModal = useCallback(() => {
+    setOpen((prev) => !prev);
+  }, [isOpen]);
+
+  return (
+    <PageContainer>
+      <HeaderSection>
+        <SocialSearch sticky />
+        <Button variant="primary" onClick={handleToggleCreationModal}>
+          <PlusIcon />{' '}
+          <span>
+            <FormattedMessage id="exploreHeader.createCommunityButton" />
+          </span>
+        </Button>
+      </HeaderSection>
+      <Recommended />
+      <CategoriesCard />
+      <FollowedEnterprises />
+      <CommunityCreationModal isOpen={isOpen} onClose={handleToggleCreationModal} />
+    </PageContainer>
+  );
+};
 
 export default ExplorePage;
