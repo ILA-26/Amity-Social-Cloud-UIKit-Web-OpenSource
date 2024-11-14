de5d645829238cd6650b6c710143e4b54d0fee6e
Abdelhak RAHMOUNI <rahmouni.abdelhak.1@gmail.com>
2024-11-14 15:41:33 +0100

    feat: add back button to navigate back to explore

--- src/social/pages/Application/index.tsx
+++ src/social/pages/Application/index.tsx
@@ -12,17 +12,25 @@ import ProfileSettings from '~/social/components/ProfileSettings';
 import { useNavigation } from '~/social/providers/NavigationProvider';
 import useSDK from '~/core/hooks/useSDK';
 import { ILA26_internalFeedProps } from '~/ila26/types/customPosts';
+import Button from '~/core/components/Button';
+import { ArrowLeftIcon } from '~/icons';
+import { FormattedMessage } from 'react-intl';
 
 const ApplicationContainer = styled.div`
   height: 100%;
   width: 100%;
 `;
 
+const BackIcon = styled(ArrowLeftIcon)`
+  width: 10px;
+  margin-right: 10px;
+`;
+
 const Community = ({
   ILA26_getInternalData,
   ILA26_communityManagerProps,
 }: ILA26_internalFeedProps) => {
-  const { page } = useNavigation();
+  const { page, onClickCommunity } = useNavigation();
 
   const { client } = useSDK();
   const [socialSettings, setSocialSettings] = React.useState<Amity.SocialSettings | null>(null);
@@ -43,7 +51,17 @@ const Community = ({
   return (
     <ApplicationContainer>
       {
-        (page.type === PageTypes.Explore || page.type === PageTypes.NewsFeed) && <ExplorePage /> // hide community news feed and fallback to explore
+        page.type === PageTypes.Explore || page.type === PageTypes.NewsFeed ? (
+          <ExplorePage />
+        ) : (
+          <Button
+            variant="secondary"
+            onClick={() => onClickCommunity(undefined as unknown as string)}
+          >
+            <BackIcon />
+            <FormattedMessage id="backTitle" />
+          </Button>
+        ) // hide community news feed and fallback to explore
       }
 
       {page.type === PageTypes.CommunityFeed && (
