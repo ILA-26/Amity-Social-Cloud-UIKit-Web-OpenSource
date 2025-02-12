07bdf11eba3a8f8739628c75ce41e5c530eb34d8
Yacine Bakhti <yacinebakhti1mi@gmail.com>
2025-02-12 15:07:42 +0100

    feat:Add espagnol language support

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,5 @@
 import en from './en.json';
 import fr from '../ila26/i18n/fr.json';
-import ar from '../ila26/i18n/fr.json';
-export default { en, fr, ar };
+import ar from '../ila26/i18n/ar.json';
+import es from '../ila26/i18n/es.json';
+export default { en, fr, ar, es };
--- src/ila26/types/kitProviderProps.ts
+++ src/ila26/types/kitProviderProps.ts
@@ -1,5 +1,5 @@
 type ILA26_UiKitProviderProps = {
-  locale: 'en-US' | 'fr-FR' | 'ar';
+  locale: 'en-US' | 'fr-FR'  | 'es-ES' | 'ar';
 };
 
 export { ILA26_UiKitProviderProps };
